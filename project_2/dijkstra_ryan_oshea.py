e0211e6fa6113b91cbc369ef59266a8c6ee0ca71
Ryan O'Shea <rpo1202@gmail.com>
2024-03-03 23:27:20 -0500

    Added project 2 skeleton

---
+++ project_2/dijkstra_ryan_oshea.py
@@ -0,0 +1,17 @@
+import cv2
+
+class dijkstraMapSolver():
+    def __init__(self):
+        pass
+    
+    # Makes the map image based on the parameters defined in the assigment
+    def makeMap(self):
+        pass
+    
+    # Gets the start and end point from user input and stores them
+    def getStartAndGoalInput(self):
+        pass
+    
+    # Finds the shortest path from the start to end goal using Dijkstra's algorithm
+    def findPath(self):
+        pass
